49b61265a80d7cf30ce7bd92de6d0efd85517a00
Muhammad Nabeel <nabeelchipa@gmail.com>
2024-10-31 22:50:25 -0700

    exercise 2

--- classes-objects.py
+++ classes-objects.py
@@ -13,3 +13,29 @@
 
 # car2 = Car()
 # car2.basicFeatures('Sliver', 4, 2, 'BMW')
+
+
+# Exercise 2
+class BankAccount:
+    def set_details(self, name, balance = 0 ):
+        self.name = name
+        self.balance = balance
+
+    def display(self):
+        print('Name:', self.name)
+        print('Balance:', self.balance)
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def deposit(self, amount):
+        self.balance += amount
+
+account1 = BankAccount()
+account1.set_details('Nabeel', 1000)
+
+account1.display()
+
+account1.withdraw(200)
+account1.deposit(500)
+account1.display()
