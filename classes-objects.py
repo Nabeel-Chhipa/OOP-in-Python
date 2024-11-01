1dd13391c29ba95c0f6dffd1ff1a1623ca0a98d0
Muhammad Nabeel <nabeelchipa@gmail.com>
2024-10-31 22:15:10 -0700

    classes & objects (Exercise 1)

---
+++ classes-objects.py
@@ -0,0 +1,15 @@
+# Exercise 1
+
+# class Car:
+#     def basicFeatures(self, color, wheels, seats, company):
+#         self.color = color
+#         self.wheels = wheels
+#         self.seats = seats
+#         self.company = company
+#         print('Color: ', self.color, '\nWheels: ', self.wheels, '\nSeats: ', self.seats, '\nCompany: ', self.company)
+
+# car1 = Car()
+# car1.basicFeatures('Black', 4, 5, 'Hoda')
+
+# car2 = Car()
+# car2.basicFeatures('Sliver', 4, 2, 'BMW')
